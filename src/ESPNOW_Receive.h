f0715b98d4f853cd07f70dba57930ccf3cfc69a6
SomewhatJoe <joehathway99@gmail.com>
2023-01-13 19:00:42 +0000

    Update var names

--- src/ESPNOW_Receive.h
+++ src/ESPNOW_Receive.h
@@ -1,12 +1,12 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-void *ptr = nullptr;
+void *_ptr = nullptr;
 
 template<typename T>
 void espReceive(T &_message)
 {
-    ptr = &_message;
+    _ptr = &_message;
     
     WiFi.mode(WIFI_STA);
 
@@ -19,6 +19,6 @@ void espReceive(T &_message)
     esp_now_register_recv_cb(
         [](const uint8_t *_mac, const uint8_t *_incomingData, int _len)
         {
-            memcpy(ptr, _incomingData, sizeof(*(T*)ptr));
+            memcpy(_ptr, _incomingData, sizeof(*(T*)_ptr));
         });
 }
